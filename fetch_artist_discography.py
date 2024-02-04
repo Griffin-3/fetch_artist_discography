ec90d0ebd854a09fa6b14183688dad37f3d5e09e
= <=>
2024-02-04 12:00:00 -0600

    v0.03 - Better oauth instruction, mentions ffmpeg, fixes ` -> ', finds .m4a files, ignores Null in .json

--- fetch_artist_discography.py
+++ fetch_artist_discography.py
@@ -1,20 +1,21 @@
 #!/bin/python3
 #REQUIREMENTS: pip install ytmusicapi yt-dlp sanitize_filename sty
-#run ytmusicapi oauth ro get oauth.json
-#version 0.2
+#requires apt install ffmpeg
+#run ytmusicapi oauth to get oauth.json
+#version 0.3
 
 import json, sys, os, subprocess, time, random, datetime
 from sanitize_filename import sanitize
 from sty import fg, rs
 from ytmusicapi import YTMusic
 
-def dumpr(r): #dump pretty json to file
+def dump_json(r): #dump pretty json to file
   o = json.dumps(r, indent=2)
   with open("temp.json", "w") as f:
     f.write(o)
 
-def sane_fn(f): #sanitize filenmae
-  return sanitize(f.replace('/','-'))
+def sane_fn(f): #sanitize filename
+  return sanitize(f.replace('/','-').replace('`',"'"))
   
 def delay(s=10): #delay plus or minus 50%
   a = int(s/2)
@@ -26,15 +27,12 @@ if len(sys.argv) == 1:
   print(f'USAGE: python3 {sys.argv[0]} "artist"')
   sys.exit()
 
-# ~ if not os.path.exists("oauth.json"):
-  # ~ u = subprocess.check_call("ytmusicapi oauth", shell=True)
-  # ~ if u != 0:
-    # ~ print("ERROR:, could not get oauth.  Try again?")
-    # ~ sys.exit()
+if not os.path.exists("oauth.json"):
+  print("cannot find oauth.json.  Please run\nytmusicapi oauth\non the command line to generate the file.")
+  sys.exit()
 
 ytm = YTMusic("oauth.json")
 r = ytm.search(sys.argv[1], filter="artists")
-o = json.dumps(r, indent=2)
 
 #if a close enought to artist ===== DEBUG
 artist = r[0]["artist"]
@@ -47,6 +45,7 @@ discography_id = r["albums"]["browseId"]
 discography_params = r["albums"]["params"]
 
 r = ytm.get_artist_albums(discography_id, discography_params)
+
 num_albums = len(r)
 errors = 0
 b = 0
@@ -61,30 +60,44 @@ for a in r: #for each album in dicography
   print(f"{b}/{num_albums} -- {album_title}")
   
   s = ytm.get_album(album_id)
+  # ~ try: # ===== trying to identify live albums
+    # ~ print(s["description"])
+  # ~ except:
+    # ~ pass
   tracks = s["trackCount"]
   for n in range(tracks): #for each track in album
     t = s["tracks"][n]
     song_title = t["title"]
     song_id = t["videoId"]
-    song_sane = sane_fn(song_title)
-    song_file = f"{n} - {song_sane}"
-    song_filename = f"{path}/{song_file}"
-    if os.path.exists(song_filename+".opus"):
-      print(f"{fg.li_blue}SKIPPED{fg.rs}")
-    else:
-      cmd = f'yt-dlp -q -x -P "{path}" -o "{song_file}" -- {song_id} '
-      u = subprocess.check_call(cmd, shell=True)
-      if u != 0: #result code
-        errors = errors + 1
-        print(f"{fg.red}FAIL{fg.rs} - {song_file}")
-      else:  
-        print(f"{fg.green}GOOD{fg.rs} - {song_file}")
-        c = c + 1
-      if errors == 3:
-        print ("STOP == too many errors!")
-        sys.exit()
-      delay(2)
+    if song_id:
+      song_sane = sane_fn(song_title)
+      song_file = f"{n} - {song_sane}"
+      song_filename = f"{path}/{song_file}"
+      if os.path.exists(song_filename+".opus") or os.path.exists(song_filename+".m4a"):
+        print(f"{fg.li_blue}SKIPPED{fg.rs}")
+      else:
+        cmd = f'yt-dlp -q -x -P "{path}" -o "{song_file}" -- {song_id} '
+        try:
+          u = subprocess.check_call(cmd, shell=True)
+        except:
+          print(cmd) #uncaught error
+          dump_json(s)
+          sys.exit()
 
+        if u != 0: #result code
+          errors = errors + 1
+          print(f"{fg.red}FAIL{fg.rs} - {song_file}")
+        else:  
+          print(f"{fg.green}GOOD{fg.rs} - {song_file}")
+          c = c + 1
+        if errors == 3:
+          print ("STOP == too many errors!")
+          sys.exit()
+        delay(2)
+    else: #song_id = Null
+      c = c + 1
+      print(f"{fg.red}NULL{fg.rs} - {song_file}")
+      
 end = time.time()
 elapsed = int(end - start)
 hms = str(datetime.timedelta(seconds=(elapsed)))
